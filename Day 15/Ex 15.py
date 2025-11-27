623bd487fcf9b8e51c81373f65ccaba65db1af86
RahulN565304 <rahul.n565304@gmail.com>
2025-11-27 12:34:57 +0530

    Sequence of for loop

---
+++ Day 15/Ex 15.py
@@ -0,0 +1,18 @@
+for i in range(11):
+    print("A", end="")
+for j in range(8):
+    print("B", end="")
+for i in range(5):
+    if i % 2 == 0:
+        print("C", end="")
+    else:
+        print("D", end="")
+print("E", end="")
+
+for i in range(9):
+    if i<5:
+        print("F", end="")
+    else:
+        print("G", end="")
+
+
