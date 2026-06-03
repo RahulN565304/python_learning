7f9d59064f5df22bd43d0f27eba034202ea5e77b
RahulN565304 <rahul.n565304@gmail.com>
2026-06-03 11:18:13 +0530

    Multi Inheritance

---
+++ Day 38/Ex.38.py
@@ -0,0 +1,39 @@
+class Animal():
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def eat(self):
+        print(f"{self.name}  is eating")
+        
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+        
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+        
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+        
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Cubo")
+hawk = Hawk("Jeremy")
+fish = Fish("Meg")
+
+rabbit.eat()
+hawk.hunt()
+fish.sleep()
+fish.eat()
+fish.sleep()
+fish.flee()
+fish.hunt()
